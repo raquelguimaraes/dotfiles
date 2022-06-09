32f543b77501e21d147c6ce5eefc7fefe2b4ed33
Raquel Guimaraes <raquelrag@gmail.com>
2022-06-09 21:23:31 +0200

    focusing on zsh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,15 @@
-#!/bin/bash
-
 function create_sym_links {
     echo "Creating Sym Links"
-    ln -sf `pwd`/.bash_prompt ~
-    ln -sf `pwd`/.bash_aliases ~
-    ln -sf `pwd`/.bash_misc ~
+    # ln -sf `pwd`/.aliases.zsh "$ZSH_CUSTOM/aliases.zsh"
     ln -sf `pwd`/.gitconfig ~
     ln -sf `pwd`/.tmux.conf ~
     ln -sf `pwd`/.vimrc ~
-}
-
-function add_source_to_profile {
-    echo "Adding sources to .profile"
-    echo "source ~/.bash_prompt" >> ~/.profile
-    echo "source ~/.bash_aliases" >> ~/.profile
-    echo "source ~/.bash_misc" >> ~/.profile
 }
 
 while getopts ":sp" opt; do
   case $opt in
     s)
       create_sym_links
-      ;;
-    p)
-      add_source_to_profile
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
