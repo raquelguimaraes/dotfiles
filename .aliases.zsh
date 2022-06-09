ee253c1f0b7d1576168867568d56cdf052abad79
Raquel Guimaraes <raquelrag@gmail.com>
2022-06-09 21:27:58 +0200

    new aliases

--- .aliases.zsh
+++ .aliases.zsh
@@ -8,3 +8,10 @@ alias tls='tmux ls'
 alias tat='tmux attach -t'
 alias tns='tmux new-session -s'
 alias tks='tmux kill-session -t'
+
+alias gti='git'
+alias git-pull-all='find . -name .git -type d -execdir git pull -r -v ";"'
+
+alias dstop='docker stop $(docker ps -a -q)'
+alias drm='docker rm $(docker ps -a -q)'
+alias drun='(){docker run -it -v `pwd`:/data $1 /bin/bash;}'
